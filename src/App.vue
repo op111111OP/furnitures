456425214e3424eedc0283843adbfb0e7203f55b
op111111OP <90711384+op111111OP@users.noreply.github.com>
2023-12-06 22:40:12 +0200

    Add files via upload

---
+++ src/App.vue
@@ -0,0 +1,170 @@
+<script setup>
+import Carusel from './components/Carusel.vue';
+import imgSebastian from './assets/img/Sebastian.jpg'
+</script>
+
+<template>
+   <div class="wrapper">
+      <header>
+         <div class="text_header">ДИЗАЙНЕРСЬКІ СТІЛЬЦІ</div>
+      </header>
+      <div class="box_carusel">
+         <Carusel />
+      </div>
+   </div>
+   <div class="box_text">
+      <div class="box_box_text">
+         <img class="backdrop-img" :src="imgSebastian" alt="">
+         <div class="text">Дизайнерські стільці - це те, чим я завжди хотів займатися у своєму<br>
+            житті.<br>
+            Я відмінно їх виготовляю та готовий виконати будь-яке ваше побажання<br>
+            або виберіть<br>
+            із тих,що представлені на моєму сайті. Гарантую швидке та якісне<br>
+            виконання. Ви залишитесь задоволені. Дякую за ваш наступний дзвінок! </div>
+         <div class="contact">
+            <div class="contact_text">Контакти
+            </div>
+            <div class="num_el">
+               <div class="num">+380 97 8037 598</div>
+               <div class="el">ivansaparenko4@gmail.com</div>
+            </div>
+            <div class="atres">вулиця Нестерова, 5, Харків, Харківська область, 61032</div>
+         </div>
+      </div>
+   </div>
+</template>
+
+<style scoped>
+header {
+   width: 100%;
+   height: 56px;
+   background: #D9D9D9;
+   display: flex;
+   justify-content: center;
+}
+
+
+.box_carusel {
+   background: #D9D9D9;
+   margin-top: 30px;
+   padding-top: 20px;
+
+}
+
+.text_header {
+   margin: auto;
+   font-family: url("./assets/IstokWeb-Bold.ttf") format("ttf");
+   font-style: normal;
+   font-weight: 700;
+   font-size: 32px;
+   line-height: 46px;
+   color: #000000;
+}
+
+.wrapper {
+   flex: 1;
+}
+
+.box_text {
+   width: 100vw;
+   margin-top: 15px;
+   display: flex;
+   justify-content: center;
+}
+
+.box_box_text {
+   position: relative;
+   padding-bottom: 50px;
+   width: 90%;
+   background: #3a3a3a;
+   display: flex;
+   align-items: center;
+   flex-direction: column;
+}
+
+.backdrop-img {
+   opacity: 0.3;
+   position: absolute;
+   top: 0;
+   left: 0;
+   width: 100%;
+   min-height: 100%;
+}
+
+.text {
+   padding: 20px 30px;
+   font-family: url("./assets/IstokWeb-Bold.ttf") format("ttf");
+
+   z-index: 10;
+   font-family: 'Istok Web';
+   font-style: normal;
+   font-weight: 400;
+   font-size: 32px;
+   line-height: 42px;
+   text-align: center;
+
+   color: #FFFFFF;
+
+
+}
+
+
+.contact {
+   font-family: url("./assets/IstokWeb-Bold.ttf") format("ttf");
+
+   z-index: 10;
+   display: flex;
+   flex-direction: column;
+   align-items: center;
+   width: 80%;
+   gap: 10px;
+   color: #ffffff;
+   font-size: 24px;
+}
+
+.num_el {
+   display: flex;
+   justify-content: space-between;
+   width: 70%;
+}
+
+.contact_text {
+   font-size: 40px;
+   margin: 45px 0;
+}
+
+.atres {
+   margin-top: 30px;
+}
+
+@media screen and (max-width: 900px) {
+   .text_header {
+
+      font-size: 25px;
+
+   }
+
+   .text {
+
+      font-size: 25px;
+
+   }
+
+   .contact_text {
+      font-size: 30px;
+      margin: 25px 0;
+   }
+
+   .contact {
+
+
+      font-size: 18px;
+   }
+
+   .num_el {
+      display: flex;
+      flex-direction: column;
+      width: 70%;
+   }
+}
+</style>
---
+++ src/components/Carusel.vue
@@ -0,0 +1,138 @@
+<template>
+   <Carousel :breakpoints="breakPrise" :itemsToShow="3" :wrapAround="true" :transition="500">
+      <Slide v-for="(item, index) in CarouselSrc" :key="index">
+         <div class="carousel__item"><img :src="item.src" :alt="index" />
+            <div class="prise">{{ item.prise }} грн</div>
+         </div>
+      </Slide>
+
+      <template #addons>
+         <Navigation />
+         <Pagination />
+      </template>
+   </Carousel>
+</template>
+<script>
+import { defineComponent } from 'vue'
+
+import 'vue3-carousel/dist/carousel.css'
+import { Carousel, Slide, Pagination, Navigation } from 'vue3-carousel';
+import img1 from '@/assets/img/imaga1.png';
+import img2 from '@/assets/img/imaga2.png';
+import img3 from '@/assets/img/imaga3.png';
+import img4 from '@/assets/img/imaga4.png';
+import img5 from '@/assets/img/imaga5.png';
+import img6 from '@/assets/img/imaga6.png';
+import img7 from '@/assets/img/imaga7.png';
+import img8 from '@/assets/img/imaga8.png';
+
+export default defineComponent({
+   name: 'Autoplay',
+   components: {
+      Carousel,
+      Slide,
+      Pagination,
+      Navigation,
+
+
+
+   },
+   data() {
+      return {
+         CarouselSrc: [{ src: img1, prise: 8300 }, { src: img2, prise: 4100 }, { src: img3, prise: 7550 }, { src: img4, prise: 5700 }, { src: img5, prise: 9900 }, { src: img6, prise: 5200 }, { src: img7, prise: 4500 }, { src: img8, prise: 4000 }],
+         breakPrise: {
+            0: {
+               itemsToShow: 1,
+            },
+            500: {
+               itemsToShow: 2,
+            },
+
+            800: {
+               itemsToShow: 3,
+            },
+
+         },
+      };
+   },
+})
+</script>
+
+ 
+<style scoped>
+.prise {
+   position: absolute;
+   top: 85%;
+   left: 85%;
+}
+
+.carousel__item {
+   position: relative;
+   overflow: hidden;
+   min-width: 100%;
+   max-height: 100%;
+}
+
+.carousel__slide {
+   background: rgb(255, 255, 255);
+   height: 300px;
+}
+
+.carousel__viewport {
+   perspective: 2000px;
+}
+
+.carousel__track {
+   transform-style: preserve-3d;
+}
+
+.carousel__slide--sliding {
+   transition: 0.5s;
+}
+
+.carousel__slide {
+   opacity: 0.9;
+   transform: rotateY(-20deg) scale(0.9);
+}
+
+.carousel__slide--active~.carousel__slide {
+   transform: rotateY(20deg) scale(0.85);
+}
+
+.carousel__slide--prev {
+   opacity: 1;
+   transform: rotateY(20deg) scale(0.85);
+}
+
+.carousel__slide--next {
+   opacity: 1;
+   transform: rotateY(10deg) scale(0.87);
+}
+
+.carousel__slide--active {
+   opacity: 1;
+   background: linear-gradient(226.12deg, #AAAAAA -1.27%, rgba(255, 255, 255, 0) 137.82%);
+   transform: rotateY(0) scale(1.1);
+}
+
+@media screen and (max-width: 1220px) {
+
+   .prise {
+      position: absolute;
+      top: 80%;
+      left: 80%;
+
+   }
+}
+
+@media screen and (max-width: 900px) {
+
+   .prise {
+      position: absolute;
+      top: 80%;
+      left: 75%;
+
+   }
+}
+</style>
+ 
